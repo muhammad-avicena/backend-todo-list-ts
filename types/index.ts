2fc696d51d25b617d3a73f0686862fe73391a5d9
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-10-24 17:05:00 +0700

    feat: add global types

---
+++ types/index.ts
@@ -0,0 +1,39 @@
+export interface ToDoListInterface {
+  findAllToDoList(): Promise<any>;
+  createToDoList(
+    activity: string,
+    username: string,
+    priority: string
+  ): Promise<any>;
+  updateToDoList(
+    id: string,
+    activity?: string,
+    priority?: string,
+    status?: string,
+    dueDate?: string
+  ): Promise<any>;
+  deleteToDoList(id: string): Promise<any>;
+}
+
+export interface ToDolistData {
+  activity: string;
+  username: string;
+  priority: string;
+  dueDate?: string;
+  status?: string;
+  createdDate?: string;
+}
+
+export interface AuthDaoInterface {
+  loginUser(userInfo: { username: string; password: string }): Promise<any>;
+  registerUser(userInfo: {
+    username: string;
+    password: string;
+    email: string;
+  }): Promise<any>;
+}
+
+export interface UserDaoInterface {
+  findAllUsers(): Promise<any>;
+  updateRole(id: string, role: string): Promise<any>;
+}
